0f8da8e882b1f97555c461ecbf01a005a7a7db8b
Ethan Ward <ethan.ward@mycroft.ai>
2016-10-26 13:08:25 -0500

    Update with new license and intent

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,19 +1,24 @@
-# Copyright 2016 Mycroft AI, Inc.
-#
-# This file is part of Mycroft Core.
-#
-# Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Mycroft Core is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+# The MIT License (MIT)	
+# 
+# Copyright (c) 2016 Ethan Ward
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from os.path import dirname, join
 from os import listdir
@@ -23,61 +28,62 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 from mycroft.util import play_mp3
+from random import randrange
 
 __author__ = 'eward'
 
 LOGGER = getLogger(__name__)
 
 
-class DemoSkill(MycroftSkill):
+class MP3DemoSkill(MycroftSkill):
 
     def __init__(self):
-        super(DemoSkill, self).__init__(name="DemoSkill")
+        super(MP3DemoSkill, self).__init__(name="MP3DemoSkill")
         self.process = None
 
     def initialize(self):
         self.load_data_files(dirname(__file__))
 
+        # Loop thru the "mp3" folder and build an intent for
+        # each song contain therein.  Change underscores in
+        # the title to spaces.
+        #
         for name in listdir(join(dirname(__file__), "mp3")):
             name = re.sub(".mp3", "", name)
             name = re.sub("_", " ", name)
             self.register_vocabulary(name, "SongTitle")
 
-        #mouthful_intent = IntentBuilder("MouthfulIntent").\
-        #    require("MouthfulKeyword").build()
-        #self.register_intent(mouthful_intent, self.handle_mouthful_intent)
-
-        #collapse_intent = IntentBuilder("CollapseIntent").\
-        #    require("CollapseKeyword").build()
-        #self.register_intent(collapse_intent,
-        #                     self.handle_collapse_intent)
-
         play_song_intent = IntentBuilder("PlaySongIntent").\
             require("PlayKeyword").require("SongTitle").build()
         self.register_intent(play_song_intent,
                              self.handle_play_song_intent)
-
-    def handle_mouthful_intent(self, message):
-        self.speak_dialog("mouthful")
-        self.process = play_mp3(join(dirname(__file__),"MouthfulOfDiamonds.mp3")) 
-
-    def handle_collapse_intent(self, message):
-        self.speak_dialog("collapse")
-        self.process = play_mp3(join(dirname(__file__),"SpeedTheCollapse.mp3"))
+        play_music_intent = IntentBuilder("PlayMusicIntent").\
+            require("PlayKeyword").require("MusicKeyword").build()
+        self.register_intent(play_music_intent,
+                             self.handle_play_music_intent)
 
     def handle_play_song_intent(self, message):
+        # Play the song requested
         title = message.metadata.get("SongTitle")
-        self.speak_dialog("play.song", {'title': title})
+        # No need to speak the title...
+        # self.speak_dialog("play.song", {'title': title})
         title += ".mp3"
         title = re.sub(" ", "_", title)
-        self.process = play_mp3(join(dirname(__file__),"mp3",title))
+        self.process = play_mp3(join(dirname(__file__), "mp3", title))
+
+    def handle_play_music_intent(self, message):
+        # Play any song
+        mp3s = listdir(join(dirname(__file__), "mp3"))
+        index = randrange(0, len(mp3s))
+        self.process = play_mp3(join(dirname(__file__), "mp3", mp3s[index]))
 
     def stop(self):
-        if self.process:# and self.process.poll() is None:
-            self.speak_dialog('music.stop')
+        if self.process:  # and self.process.poll() is None:
+            # No reason to say "music stopped", that is obvious!
+            # self.speak_dialog('music.stop')
             self.process.terminate()
             self.process.wait()
 
 
 def create_skill():
-    return DemoSkill()
+    return MP3DemoSkill()
